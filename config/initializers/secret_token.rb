cbe2bc93b527326427e2997446234f5b1efd89f5
Esther j <jesther0103@gmail.com>
2014-01-10 13:37:35 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Avgcalc::Application.config.secret_key_base = '201de592c2d34b077cca45e7080ec1f8a532824d2f7fd0bfaf9eff0ef8407fad934f5573012465f13abafd3cdc6041d6c425f5b298caae693b86bdbefcb360cf'
